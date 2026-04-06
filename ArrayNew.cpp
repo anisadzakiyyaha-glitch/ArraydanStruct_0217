946f6ce5f9d394579ea950eb1aa524afac88b0ed
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-04-06 20:28:50 +0700

    Menampilkan data satu per satu

--- ArrayNew.cpp
+++ ArrayNew.cpp
@@ -24,3 +24,12 @@ int main(){
     cout << "Data Index 4 = ";
     cin >> kamu[4];
     cout << endl;
+      // menampilkan satu per satu
+    cout << "Data Pertama = " << kamu[0] << endl;
+    cout << "Data Kedua = " << kamu[1] << endl;
+    cout << "Data Ketiga = " << kamu[2] << endl;
+    cout << "Data Keempat = " << kamu[3] <<endl;
+    cout << "Data Kelima = " << kamu[4] << endl;
+    cout << endl;
+
+   
