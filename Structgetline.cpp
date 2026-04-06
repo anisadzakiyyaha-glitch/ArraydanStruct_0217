4d12786cd3d5d2ea4c31f77ea92484ec3da34a18
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-04-06 20:36:41 +0700

    definisi struct DetailAlamat dengan field desa dan kota

--- Structgetline.cpp
+++ Structgetline.cpp
@@ -14,24 +14,3 @@ struct Orang
     DetailAlamat alamat;
     int umur;
 };
-
-int main() {
-    Orang mhs;
-    // mengisi data
-    cout << "Mengisi Data" << endl;
-    cout << "Nama : ";
-    getline(cin ,  mhs.nama);
-    cout << "Alamat : ";
-    getline(cin , mhs.alamat.desa);
-    cout << "Alamat : ";
-    getline(cin , mhs.alamat.kota);
-    cout << "Umur : ";
-    cin >> mhs.umur;
-    cout << endl;
-    // tampilkan
-    cout << "Menampilkan Data" << endl;
-    cout << "Nama : " << mhs.nama << endl;
-    cout << "Desa : " << mhs.alamat.desa << endl;
-    cout << "Kota : " << mhs.alamat.kota << endl;
-    cout << "Umur : " << mhs.umur << endl;
-}
